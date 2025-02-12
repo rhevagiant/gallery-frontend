132d96f5d03da98c4a1233061a15c67f6f4c729e
rhevagiant <122246574+rhevagiant@users.noreply.github.com>
2025-02-12 14:29:52 +0700

    update sidebar
    logout button

--- src/component/sidebar/sidebar.jsx
+++ src/component/sidebar/sidebar.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import { Outlet, NavLink, useNavigate } from "react-router-dom";
-import { AppBar, Box, CssBaseline, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Menu, MenuItem, Toolbar, Typography, Avatar, Dialog, DialogActions, DialogContent, DialogTitle, Button } from "@mui/material";
-import { Menu as MenuIcon, ChevronLeft as ChevronLeftIcon, Dashboard as DashboardIcon, PhotoAlbum as PhotoAlbumIcon, Image as ImageIcon } from "@mui/icons-material";
-import { bluegray } from "../../themes/color"; // Atur warna sesuai dengan preferensi Anda
+import { AppBar, Box, CssBaseline, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography, Dialog, DialogActions, DialogContent, DialogTitle, Button } from "@mui/material";
+import { Menu as MenuIcon, Dashboard as DashboardIcon, PhotoAlbum as PhotoAlbumIcon, Image as ImageIcon, Logout as LogoutIcon } from "@mui/icons-material";
+import { bluegray } from "../../themes/color"; // Sesuaikan warna sesuai preferensi Anda
 
 const sidebarWidth = 220;
 
 const Sidebar = () => {
   const [activeIndex, setActiveIndex] = useState(null); // Track active index
-  const [anchorEl, setAnchorEl] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const [openSuccessDialog, setOpenSuccessDialog] = useState(false);
-  const open = Boolean(anchorEl);
   const navigate = useNavigate();
 
   const menuItems = [
@@ -20,25 +18,14 @@ const Sidebar = () => {
     { text: "Photos", path: "/photos", icon: <ImageIcon /> },
   ];
 
-  const handleMenuClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
   const handleLogoutClick = () => {
     setOpenDialog(true);
-    setAnchorEl(null);
   };
 
   const handleLogout = () => {
     sessionStorage.clear();
     setOpenDialog(false);
     setOpenSuccessDialog(true);
-    console.log("isAuthenticated:", sessionStorage.getItem("isAuthenticated"));
-    console.log("Role after logout:", sessionStorage.getItem("role"));
   };
 
   const handleCancelLogout = () => {
@@ -58,17 +45,6 @@ const Sidebar = () => {
       {/* AppBar / Navbar */}
       <AppBar position="fixed" sx={{ bgcolor: bluegray[700] }}>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{
-              marginRight: 5,
-              display: "none",
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Gallery
           </Typography>
@@ -81,16 +57,13 @@ const Sidebar = () => {
           width: sidebarWidth,
           flexShrink: 0,
           position: "fixed",
-          top: "64px", // Menambahkan jarak untuk menghindari tumpang tindih dengan navbar
+          top: "64px",
           left: 0,
-          right: 0,
-          height: "calc(100vh - 64px)", // Mengurangi tinggi sidebar agar tidak tumpang tindih dengan navbar
+          height: "calc(100vh - 64px)",
           display: "flex",
           flexDirection: "column",
           boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
-          overflow: "hidden",
           backgroundColor: "#fff",
-          paddingRight: 0, // Mengurangi jarak di sisi kanan
         }}
       >
         <Divider />
@@ -103,66 +76,38 @@ const Sidebar = () => {
               component={NavLink}
               to={item.path}
               key={index}
-              onClick={() => setActiveIndex(index)} // Set the active index on click
+              onClick={() => setActiveIndex(index)}
               sx={{
-                // Apply active item styles
-                bgcolor: activeIndex === index ? bluegray[700] : 'transparent',
-                color: activeIndex === index ? '#fff' : bluegray[700],  // Active item text color
+                bgcolor: activeIndex === index ? bluegray[700] : "transparent",
+                color: activeIndex === index ? "#fff" : bluegray[700],
               }}
             >
-              <ListItemIcon
-                sx={{
-                  color: activeIndex === index ? '#fff' : bluegray[700], // Active icon color
-                }}
-              >
+              <ListItemIcon sx={{ color: activeIndex === index ? "#fff" : bluegray[700] }}>
                 {item.icon}
               </ListItemIcon>
-              <ListItemText
-                primary={item.text}
-                sx={{
-                  color: activeIndex === index ? '#fff' : bluegray[700],  // Active item text color
-                }}
-              />
+              <ListItemText primary={item.text} />
             </ListItem>
           ))}
         </List>
 
-        {/* Spacer to push the profile avatar to the bottom */}
+        {/* Spacer to push the logout icon to the bottom */}
         <Box sx={{ flexGrow: 1 }} />
 
-        {/* Avatar Menu */}
-        <Box sx={{ padding: 2, borderTop: "1px solid #e0e0e0" }}>
-          <Avatar
-            sx={{ bgcolor: bluegray[700], cursor: "pointer", mx: "auto" }}
-            onClick={handleMenuClick}
-          >
-            A
-          </Avatar>
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleMenuClose}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "center",
-            }}
-          >
-            <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
-          </Menu>
+        {/* Logout Button */}
+        <Box sx={{ padding: 2, borderTop: "1px solid #e0e0e0", display: "flex", justifyContent: "center" }}>
+          <IconButton sx={{color: bluegray[700]}} onClick={handleLogoutClick}>
+            <LogoutIcon fontSize="medium" />
+          </IconButton>
         </Box>
       </Box>
 
-      {/* Main Content Area with Outlet */}
+      {/* Main Content Area */}
       <Box
         component="main"
         sx={{
           flexGrow: 1,
           marginLeft: `${sidebarWidth}px`,
-          paddingTop: "64px", // to avoid overlap with AppBar
+          paddingTop: "64px",
           overflowY: "auto",
           height: "100vh",
         }}
